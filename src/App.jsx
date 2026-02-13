de98597de18e079cb490ba772b286ea253a698fe
Jasmine Ismail <jasminismail95@gmail.com>
2026-02-13 20:10:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <div  className="text-red-500"> Hello world 
+        </div>
+        
+    </>
+  )
+}
+
+export default App
